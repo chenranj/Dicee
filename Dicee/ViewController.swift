274a9dc617ad52898e19b83e6d50734baf4bca75
Chenran Jin <chenranj@buffalo.edu>
2021-07-26 00:02:51 -0400

    Initial Commit

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
-
+    
+    let diceArray : [String] = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        newDice()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        newDice()
+    }
+    
+    func newDice() {
+        diceImageView1.image = UIImage(named: diceArray[Int(arc4random_uniform(6))])
+        diceImageView2.image = UIImage(named: diceArray[Int(arc4random_uniform(6))])
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if(motion == .motionShake) {
+            newDice()
+        }
+    }
+    
 }
 
